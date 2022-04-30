1445a47b6b2e9eeaee7f7c3d47bc1b659f9c480b
aleksandrs.fridmans <aleksandrs.fridmans@simlocal.com>
2022-04-30 17:08:15 +0100

    add transaction connect node replace chain logic

--- src/main/java/com/alexnail/blockchain/controllers/BlockchainController.java
+++ src/main/java/com/alexnail/blockchain/controllers/BlockchainController.java
@@ -1,13 +1,17 @@
 package com.alexnail.blockchain.controllers;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexnail.blockchain.models.Block;
+import com.alexnail.blockchain.models.Transaction;
 import com.alexnail.blockchain.services.BlockchainService;
 
 @RestController
@@ -20,21 +24,45 @@ public class BlockchainController {
     }
 
     @GetMapping("/mine_block")
-    Block mineBlock() {
+    public Block mineBlock() {
         return blockchainService.mineBlock();
     }
 
     @GetMapping("/get_chain")
-    Collection<Block> getChain() {
+    public Collection<Block> getChain() {
         return blockchainService.getChain();
     }
 
     @GetMapping("/is_valid")
-    ResponseEntity<String> isValid() {
+    public ResponseEntity<String> isValid() {
         if (blockchainService.isValid()) {
             return new ResponseEntity<>("All good. The Blockchain is valid.", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Houston, we have a problem. The Blockchain is not valid.", HttpStatus.OK);
         }
+    }
+
+    @PostMapping("/add_transaction")
+    public ResponseEntity<String> addTransaction(@RequestBody Transaction transaction) {
+        long idx = blockchainService.addTransaction(transaction);
+        return new ResponseEntity<>(String.format("This transaction will be added to Block %d", idx), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/connect_node")
+    public ResponseEntity<String> connectNode(@RequestBody List<String> nodes) {
+        if (nodes.isEmpty())
+            return new ResponseEntity<>("No node", HttpStatus.BAD_REQUEST);
+        else {
+            return new ResponseEntity<>(blockchainService.addNodes(nodes).toString(), HttpStatus.CREATED);
+        }
+    }
+
+    @GetMapping("/replace_chain")
+    public ResponseEntity<String> replaceChain() {
+        boolean isChainReplaced = blockchainService.replaceChain();
+        if (isChainReplaced)
+            return new ResponseEntity<>("The nodes had different chains so the chain was replaced by the longest one.",  HttpStatus.OK);
+        else
+            return new ResponseEntity<>("All good. The chain is the largest one.",  HttpStatus.OK);
     }
 }
--- src/main/java/com/alexnail/blockchain/services/BlockchainService.java
+++ src/main/java/com/alexnail/blockchain/services/BlockchainService.java
@@ -5,10 +5,18 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.alexnail.blockchain.models.Block;
 import com.alexnail.blockchain.models.Transaction;
@@ -17,9 +25,13 @@ import com.google.common.hash.Hashing;
 @Service
 public class BlockchainService {
 
-    private final List<Block> chain = new ArrayList<>();
+    public static final String NODE_ADDRESS = UUID.randomUUID().toString();
+
+    private List<Block> chain = new ArrayList<>();
 
     private final List<Transaction> transactions = new ArrayList<>();
+
+    private final Set<String> nodes = new HashSet<>();
 
     public BlockchainService() {
         chain.add(createBlock(1, "0"));
@@ -30,6 +42,7 @@ public class BlockchainService {
         long previousProof = previousBlock.getProof();
         long proof = proofOfWork(previousProof);
         String previousHash = hash(previousBlock);
+        addTransaction(new Transaction(NODE_ADDRESS, "Alex", BigDecimal.ONE));
         Block block = createBlock(proof, previousHash);
         chain.add(block);
         return block;
@@ -63,14 +76,9 @@ public class BlockchainService {
         block.setTimestamp(LocalDateTime.now());
         block.setProof(proof);
         block.setPreviousHash(previousHash);
-        block.setTransactions(Collections.unmodifiableList(transactions));
+        block.setTransactions(new ArrayList<>(transactions));
         transactions.clear();
         return block;
-    }
-
-    private long addTransaction(String sender, String receiver, BigDecimal amount) {
-        transactions.add(new Transaction(sender, receiver, amount));
-        return getPreviousBlock().getIndex() + 1;
     }
 
     public Collection<Block> getChain() {
@@ -78,10 +86,14 @@ public class BlockchainService {
     }
 
     public boolean isValid() {
-        Block previousBlock = chain.get(0);
+        return isValid(chain);
+    }
+
+    private boolean isValid(List<Block> chn) {
+        Block previousBlock = chn.get(0);
         int blockIndex = 1;
-        while (blockIndex < chain.size()) {
-            Block block = chain.get(blockIndex);
+        while (blockIndex < chn.size()) {
+            Block block = chn.get(blockIndex);
             if (!block.getPreviousHash().equals(hash(previousBlock)))
                 return false;
 
@@ -95,5 +107,40 @@ public class BlockchainService {
             blockIndex++;
         }
         return true;
+    }
+
+    public long addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+        return getPreviousBlock().getIndex() + 1;
+    }
+
+    public Set<String> addNodes(List<String> nodes) {
+        this.nodes.addAll(nodes);
+        return this.nodes.stream().collect(Collectors.toUnmodifiableSet());
+    }
+
+    public boolean replaceChain() {
+        List<Block> longestChain = null;
+        int maxLength = chain.size();
+
+        for (String node: nodes) {
+            // request the node for its chain
+            RestTemplate restTemplate = new RestTemplate();
+            String url = String.format("%s/get_chain", node);
+            ResponseEntity<List<Block>> response = restTemplate.exchange(url, HttpMethod.GET, null,
+                                                                         new ParameterizedTypeReference<>() {});
+            if (response.getStatusCode().is2xxSuccessful()) {
+                List<Block> nodeChain = response.getBody();
+                if (nodeChain.size() > maxLength && isValid(nodeChain)) {
+                    maxLength = nodeChain.size();
+                    longestChain = nodeChain;
+                }
+            }
+            if (Objects.nonNull(longestChain)) {
+                this.chain = longestChain;
+                return true;
+            }
+        }
+        return false;
     }
 }
